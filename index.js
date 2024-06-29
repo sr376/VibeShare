3dd29240eaaeefa1f34f8e9b56302091ab797e49
Rayella Srujan teja <64093603+sr376@users.noreply.github.com>
2024-06-29 16:34:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const { ApolloServer, PubSub } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers/index");
+// const { MONGO_URI } = require("./config.js");
+const MONGO_URI = process.env.MONGO_URI;
+
+const pubsub = new PubSub();
+
+const PORT = process.env.PORT || 5000;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub }),
+});
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected MONGODB");
+    return server.listen({ port: PORT });
+  })
+  .then((res) => {
+    console.log(`server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
